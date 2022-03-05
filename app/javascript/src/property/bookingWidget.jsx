cbc85c74096976620d50536ea9483505469cfb8a
Bobby Willmes <bobbywillmes@gmail.com>
2022-03-05 08:21:17 -0700

    payments front-end

--- app/javascript/src/property/bookingWidget.jsx
+++ app/javascript/src/property/bookingWidget.jsx
@@ -38,6 +38,29 @@ class BookingWidget extends React.Component {
       })
   }
 
+  initiateStripeCheckout = (booking_id) => {
+    return fetch(`/api/charges?booking_id=${booking_id}&cancel_url=${window.location.pathname}`, safeCredentials({
+      method: 'POST',
+    }))
+      .then(handleErrors)
+      .then(response => {
+        const stripe = Stripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`)
+        stripe.redirectToCheckout({
+          // Make the id field from the Checkout Session creation API response
+          // available to this file, so you can provide it as parameter here
+          // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+          sessionId: response.charge.checkout_session_id,
+        }).then((result) => {
+          // If `redirectToCheckout` fails due to a browser or network
+          // error, display the localized error message to your customer
+          // using `result.error.message`.
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   submitBooking = (e) => {
     if (e) { e.preventDefault(); }
     const { startDate, endDate } = this.state
@@ -57,6 +80,7 @@ class BookingWidget extends React.Component {
       .then(handleErrors)
       .then(response => {
         console.log(response)
+        return this.initiateStripeCheckout(response.booking.id)
       })
       .catch(error => {
         console.log(error)
@@ -91,7 +115,7 @@ class BookingWidget extends React.Component {
         <form onSubmit={this.submitBooking}>
           <h5>${price_per_night} <small>per night</small></h5>
           <hr />
-          <div style={{ marginBottom: focusedInput ? '400px': '2rem' }}>
+          <div style={{ marginBottom: focusedInput ? '400px' : '2rem' }}>
             <DateRangePicker
               startDate={startDate} // momentPropTypes.momentObj or null,
               startDateId="start_date" // PropTypes.string.isRequired,
